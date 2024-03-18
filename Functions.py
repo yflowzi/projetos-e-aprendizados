93be003988c32b016d9c7dbf45ded0826ed4393f
Rafael Moreira Lopes <rafael330330@gmail.com>
2024-03-18 11:08:09 -0300

    segundo commit

--- Functions.py
+++ Functions.py
@@ -1,10 +1,23 @@
-def object_car(carro):
-  print(carro)
+# def object_car(carro):
+#   print(carro)
 
 
-object_car("cor: vermelho")
-object_car("placa: 509E3")
-object_car("modelo: corsa classic")
+# object_car("cor: vermelho")
+# object_car("placa: 509E3")
+# object_car("modelo: corsa classic")
+
+
+#criei uma função objeto carro com 3 argumentos
+def meu_carro (placa, cor, modelo):
+  print("modelo:",modelo,"cor:", cor,"placa", placa)
+
+#chamei essa função duas vezes
+meu_carro("509e35","red", "monza 1989")
+meu_carro("88888", "blue", "opala")
+
+#ao executar o codigo ira aparecer a placa a cor e o modelo do carro.
+
+
 
 
 
