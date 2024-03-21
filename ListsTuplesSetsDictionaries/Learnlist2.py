7ba73fdae0866577658315b2d7d34d74d35e78ea
Rafael Moreira Lopes <rafael330330@gmail.com>
2024-03-21 11:44:37 -0300

    atualização diaria

---
+++ ListsTuplesSetsDictionaries/Learnlist2.py
@@ -0,0 +1,14 @@
+# remove
+# pop
+# del
+# clear
+
+list2 = ["apple", "banana", "cherry", "orange", "kiwi", "melon","carrot","beans", "rice"]
+for x in range(list2):
+    print(x)
+
+# list2.remove("banana") #segundo
+# list2.pop(0) #primeiro
+# list2.pop()#ultimo
+# list2.pop()#penultimo
+# print(list2)
