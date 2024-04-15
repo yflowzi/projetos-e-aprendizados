41dd36bb9d780de231b95eb5ba8a607dedc5f140
Rafael Moreira Lopes <rafael330330@gmail.com>
2024-04-15 17:12:30 -0300

    revisão

---
+++ RoadMap/newHardsSkills/StringMod.py
@@ -0,0 +1,32 @@
+def uppercase():
+    x = "hello world in upper case"
+    if x != int:
+        print(x.upper())
+        
+
+
+y = uppercase()
+    
+def lowecase():
+    t1 = "HELLO WRLD IN UPPER CASE"
+    if t1 != str:
+        print(t1.lower())
+
+y1 = lowecase()
+
+#remove whitespace follow my code example bellow:
+
+def whitespacedelete():
+    t2 = "h e l l o / w o r l d"
+    if t2 != str:
+        print(t2.strip())
+
+y2 = whitespacedelete()
+
+def replacestr():
+    a = "Hello, World!"
+    if a != str:
+        print(a.replace("H", "J"))
+
+a1 = replacestr()
+
