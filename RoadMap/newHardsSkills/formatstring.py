71075e86de1f14572cfb57eb2bcac06a4ae8d0c4
Rafael Moreira Lopes <rafael330330@gmail.com>
2024-04-15 18:19:49 -0300

    format strings and  escape characters

---
+++ RoadMap/newHardsSkills/formatstring.py
@@ -0,0 +1,22 @@
+#Uma f string pode conter variáveis, operações, funções e modificadores para formatar o valor.
+price = 59
+
+txt1 =f"essa calça custa: {price}R$"
+
+txt2 =f"essa camiseta custa o dobro do valor da calça: {price*2}R$"
+
+print(f"{txt1}\n{txt2}")
+
+#Escape Characters
+
+    #Other escape characters used in Python:
+    #Code 	Result
+    #\' 	Single Quote 	
+    #\\ 	Backslash 	
+    #\n 	New Line 	
+    #\r 	Carriage Return 	
+    #\t 	Tab 	
+    #\b 	Backspace 	
+    #\f 	Form Feed 	
+    #\ooo 	Octal value 	
+    #\xhh 	Hex value
